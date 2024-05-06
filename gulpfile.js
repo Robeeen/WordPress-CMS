66e568375f27f63cd83b815b88ce99983746a1f6
Robeeen <robeeen@gmail.com>
2024-05-06 16:34:30 +0600

    updates

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+/*
+-----------------------------------
+var module = require('module');
+gulp.task - define task,
+gulp.src - points to file or folders
+gulp.dest - where to place the complied files.
+gulp.watch - watch files folders
+---------------------------------
+*/
+const gulp = require('gulp');
+//const sass = require('gulp-sass');
+//const autoprefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+
+gulp.task('styles', function() {
+    return gulp.src('./devwp/src/scss/**/*.scss')
+      .pipe(sass().on('error', sass.logError))
+      //.pipe(autoprefixer('last 2 versions'))
+      .pipe(cleanCSS())
+      .pipe(rename({ suffix: '.min' })) 
+      .pipe(gulp.dest('./devwp/dist/css')) 
+      .pipe(browserSync.reload({      
+           stream: true     
+       }));
+});
+gulp.task('scripts', function() {
+    return gulp.src('./devwp/src/js/*.js')
+      .pipe(concat('all.js'))
+      .pipe(uglify())
+      .pipe(rename({ suffix: '.min' }))
+      .pipe(gulp.dest('./devwp/dist/js'))
+      .pipe(browserSync.reload({
+           stream: true
+       }));
+}); 
+gulp.task('watch', function() {   
+    browserSync.init({
+    injectChanges: true,
+       proxy: "http://devwp.local/"
+});
+gulp.watch('./devwp/src/scss/**/*.scss', gulp.series('styles'));
+gulp.watch('./devwp/src/js/*.js', gulp.series('scripts'));
+gulp.watch('**/*.php').on('change', browserSync.reload); });
+gulp.task('default', gulp.parallel('styles', 'scripts', 'watch'));
---
+++ readme.md
@@ -0,0 +1,24 @@
+1. after installation of gulp and configure to execute from terminal, if you place gulp command it will say
+"Running script is disabled on this system". So you need to disable Execution_policy by unrestricted command from
+PowerShell terminal.
+
+2. gulpfile.js needs to be created to execute all tasks to automate.
+
+/*
+-----------------------------------
+var module = require('module');
+gulp.task - define task,
+gulp.src - points to file or folders
+gulp.dest - where to place the complied files.
+gulp.watch - watch files folders
+---------------------------------
+*/
+
+3. How to install scss in gulp for wordpress
+
+npm install --save-dev gulp-sass
+npm install saas gulp-sass --save-dev
+npm install gulp-cli --save-dev
+
+
+
