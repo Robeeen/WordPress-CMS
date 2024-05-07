bea9b15be3be8c36819bad1ab7971368d3fd6274
Robeeen <robeeen@gmail.com>
2024-05-07 20:00:04 +0600

    updated

--- devwp/header.php
+++ devwp/header.php
@@ -1,3 +1,13 @@
 <?php
-
-echo "hello";
+/* 
+Header Template for entire theme
+Header for devwp theme.
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<title><?php wp_title( '|', true, 'right' ); ?></title>
+<?php wp_head(); ?>
+</head>
